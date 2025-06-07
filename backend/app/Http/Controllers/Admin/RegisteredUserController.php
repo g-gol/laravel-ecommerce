9aa25cf9a1a3d64706027f68bed6cd0b62e89218
George Golubev <golubev.zhr@gmail.com>
2025-06-07 07:34:54 +0300

    feat(admin): store action for registration

--- backend/app/Http/Controllers/Admin/RegisteredUserController.php
+++ backend/app/Http/Controllers/Admin/RegisteredUserController.php
@@ -3,13 +3,42 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Contracts\View\View;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\View\View;
+
 
 class RegisteredUserController extends Controller
 {
     public function create(): View
     {
         return view('auth.register');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'username' => ['required', 'string', 'max:255'],
+            'email' => ['required', Rules\Email::default(), 'unique:' . User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+
+        $user = User::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->string('password')),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect('/');
     }
 }
