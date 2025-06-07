83ea666bea595b29858b1da358f9b73f9476d5de
George Golubev <golubev.zhr@gmail.com>
2025-06-07 07:49:33 +0300

    feat(admin): store and destroy actions for session

--- backend/app/Http/Controllers/Admin/SessionController.php
+++ backend/app/Http/Controllers/Admin/SessionController.php
@@ -3,13 +3,37 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class SessionController extends Controller
 {
-     public function create(): View
-        {
-            return view('auth.login');
-        }
+    public function create(): View
+    {
+        return view('auth.login');
+    }
+
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        return redirect('home');
+    }
+
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('admin.login.form');
+    }
 }
