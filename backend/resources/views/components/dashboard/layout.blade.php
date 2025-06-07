d636a749b98e61f2d869fcbd7c56fbe0ae9f6889
George Golubev <golubev.zhr@gmail.com>
2025-06-07 10:57:06 +0300

    feat(admin): slot for title in blade

--- backend/resources/views/components/dashboard/layout.blade.php
+++ backend/resources/views/components/dashboard/layout.blade.php
@@ -1,4 +1,5 @@
 <x-default-layout>
+    <x-slot name="title">{{ $title ?? 'Dashboard' }}</x-slot>
     <x-dashboard.sidebar/>
     <div class="sm:ml-64">
         <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg bg-gray-50 min-h-screen">
--- backend/resources/views/dashboard/index.blade.php
+++ backend/resources/views/dashboard/index.blade.php
@@ -1,5 +1,5 @@
 <x-dashboard.layout>
-
+<x-slot name="title">Dashboard</x-slot>
     <div class="flex flex-col items-center h-48 mb-4 rounded-sm ">
         asdfasdf
     </div>
