6fc7993ec849d0ddbcdb0bca3c563eca4306ee6d
George Golubev <golubev.zhr@gmail.com>
2025-06-11 08:04:13 +0300

    feat(admin,user): eager load users list

--- backend/app/Http/Controllers/Admin/UserController.php
+++ backend/app/Http/Controllers/Admin/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -11,7 +12,17 @@ class UserController extends Controller
 {
     public function index(): View
     {
-        $users = User::query()->latest()->paginate(10)->withQueryString();
+        $users = User::query()->latest()->with(['roles'])->paginate(10)->withQueryString();
         return view('users.index', ['users' => $users]);
+    }
+
+    public function edit(User $user): View
+    {
+        return view('users.edit', ['user' => $user]);
+    }
+
+    public function update(Request $request): RedirectResponse
+    {
+        return redirect()->back();
     }
 }
