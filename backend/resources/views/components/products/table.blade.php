b1f3eb55ee878d17528a707aa4bd3c263c32db15
George Golubev <golubev.zhr@gmail.com>
2025-06-13 11:33:52 +0300

    feat(admin,blade): products index and table

---
+++ backend/resources/views/components/products/table.blade.php
@@ -0,0 +1,52 @@
+@props(['products'])
+<div class="relative shadow-md sm:rounded-lg w-3/4">
+    <x-table.search/>
+    <x-table.layout>
+        <x-table.thead>
+            <x-table.th>
+                Name
+            </x-table.th>
+            <x-table.th>
+                Status
+            </x-table.th>
+            <x-table.th>
+                Price
+            </x-table.th>
+            <x-table.th>
+                Action
+            </x-table.th>
+        </x-table.thead>
+        <x-table.tbody>
+            @foreach ($products as $product)
+                <x-table.row>
+                    <x-table.td class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
+                        <a href="{{ route('admin.products.show', $product) }}" class="underline text-base font-semibold">{{ $product->name }}</a>
+                    </x-table.td>
+                    <x-table.td>
+                        {{ $product->status }}
+                    </x-table.td>
+
+                    <x-table.td>
+                        {{ $product->price }}
+                    </x-table.td>
+
+                    <x-table.td>
+                        <a href="{{ route('admin.products.edit', $product) }}"
+                           class="font-medium text-blue-600 hover:underline">Edit</a>
+                    </x-table.td>
+
+                    <x-table.td>
+                        @can('delete-product')
+                            <form action="{{ route('admin.products.delete', $product) }}" method="post">
+                                @csrf
+                                @method('delete')
+                                <button class="font-medium text-red-600 hover:underline" type="submit">Delete</button>
+                            </form>
+                        @endcan
+                    </x-table.td>
+                </x-table.row>
+            @endforeach
+        </x-table.tbody>
+    </x-table.layout>
+    {{ $products->links('vendor.pagination.tailwind') }}
+</div>
--- backend/resources/views/products/index.blade.php
+++ backend/resources/views/products/index.blade.php
@@ -1,6 +1,6 @@
 <x-dashboard.layout>
-    <x-slot name="title">Manage users</x-slot>
+    <x-slot name="title">Manage products</x-slot>
     <div class="flex flex-col items-center h-48 mb-4 rounded-sm w-full">
-        <x-users.table :users="$users"/>
+        <x-products.table :products="$products"/>
     </div>
 </x-dashboard.layout>
