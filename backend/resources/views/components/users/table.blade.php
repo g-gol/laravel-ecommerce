648ddbc84996a5f908b3e664c2fa1c55756e1537
George Golubev <golubev.zhr@gmail.com>
2025-06-13 08:17:53 +0300

    feat(admin,user): not showing delete button if user is auth user, also some line formatting

--- backend/resources/views/components/users/table.blade.php
+++ backend/resources/views/components/users/table.blade.php
@@ -6,80 +6,84 @@
         <div class="relative">
             <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
                 <svg class="w-4 h-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 20 20">
+                     viewBox="0 0 20 20">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                          d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                 </svg>
             </div>
             <input type="text" id="table-search-users"
-                class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-                placeholder="Search for users">
+                   class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                   placeholder="Search for users">
         </div>
     </div>
     <table class="w-full text-sm text-left rtl:text-right text-gray-500">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
-            <tr>
-                <th scope="col" class="p-4">
-                    <div class="flex items-center">
-                        <input id="checkbox-all-search" type="checkbox"
-                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500">
-                        <label for="checkbox-all-search" class="sr-only">checkbox</label>
-                    </div>
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Name
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Role
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Email verified
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Action
-                </th>
-            </tr>
+        <tr>
+            <th scope="col" class="p-4">
+                <div class="flex items-center">
+                    <input id="checkbox-all-search" type="checkbox"
+                           class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500">
+                    <label for="checkbox-all-search" class="sr-only">checkbox</label>
+                </div>
+            </th>
+            <th scope="col" class="px-6 py-3">
+                Name
+            </th>
+            <th scope="col" class="px-6 py-3">
+                Role
+            </th>
+            <th scope="col" class="px-6 py-3">
+                Email verified
+            </th>
+            <th scope="col" class="px-6 py-3">
+                Action
+            </th>
+        </tr>
         </thead>
         <tbody>
-            @foreach ($users as $user)
-                <tr class="bg-white border-b border-gray-200 hover:bg-gray-50 ">
-                    <td class="w-4 p-4">
-                        <div class="flex items-center">
-                            <input id="checkbox-table-search-1" type="checkbox"
-                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                            <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+        @foreach ($users as $user)
+            <tr class="bg-white border-b border-gray-200 hover:bg-gray-50 ">
+                <td class="w-4 p-4">
+                    <div class="flex items-center">
+                        <input id="checkbox-table-search-1" type="checkbox"
+                               class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                    </div>
+                </td>
+                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
+                    <div class="ps-3">
+                        <div class="text-base font-semibold">{{ $user->username }}</div>
+                        <div class="font-normal text-gray-500">{{ $user->email }}</div>
+                    </div>
+                </th>
+                <td class="px-6 py-4">
+                    {{ $user->roles->pluck('name')->implode(', ') }}
+                </td>
+                <td class="px-6 py-4">
+                    <div class="flex items-center">
+                        <div
+                            class="h-2.5 w-2.5 rounded-full me-2 {{ $user->email_verified_at ? 'bg-green-500' : 'bg-red-400' }}">
                         </div>
-                    </td>
-                    <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
-                        <div class="ps-3">
-                            <div class="text-base font-semibold">{{ $user->username }}</div>
-                            <div class="font-normal text-gray-500">{{ $user->email }}</div>
-                        </div>
-                    </th>
-                    <td class="px-6 py-4">
-                        {{ $user->roles->pluck('name')->implode(', ') }}
-                    </td>
-                    <td class="px-6 py-4">
-                        <div class="flex items-center">
-                            <div
-                                class="h-2.5 w-2.5 rounded-full me-2 {{ $user->email_verified_at ? 'bg-green-500' : 'bg-red-400' }}">
-                            </div>
-                            {{ $user->email_verified_at ?? 'Not verified' }}
-                        </div>
-                    </td>
-                    <td class="px-6 py-4">
-                        <a href="{{ route('admin.users.edit', $user) }}"
-                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
-                    </td>
-                    <td class="px-6 py-4">
+                        {{ $user->email_verified_at ?? 'Not verified' }}
+                    </div>
+                </td>
+                <td class="px-6 py-4">
+                    <a href="{{ route('admin.users.edit', $user) }}"
+                       class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
+                </td>
+                <td class="px-6 py-4">
+                    @if($user->id !== auth()->id())
                         <form action="{{ route('admin.users.delete', $user) }}" method="post">
                             @csrf
                             @method('delete')
                             <button class="font-medium text-red-600 hover:underline" type="submit">Delete</button>
                         </form>
-                    </td>
-                </tr>
-            @endforeach
+                    @else
+                        <span>You</span>
+                    @endif
+                </td>
+            </tr>
+        @endforeach
         </tbody>
     </table>
     {{ $users->links() }}
--- backend/resources/views/users/index.blade.php
+++ backend/resources/views/users/index.blade.php
@@ -1,6 +1,5 @@
 <x-dashboard.layout>
     <x-slot name="title">Manage users</x-slot>
-    sdfasfd
     <div class="flex flex-col items-center h-48 mb-4 rounded-sm w-full">
         <x-users.table :users="$users"/>
     </div>
