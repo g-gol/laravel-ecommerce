8585a94b2cf7ea22001a26e3ed83cd5098a54857
George Golubev <golubev.zhr@gmail.com>
2025-06-14 12:04:08 +0300

    feat(blade): merging class attribute for form layout

--- backend/resources/views/components/form/layout.blade.php
+++ backend/resources/views/components/form/layout.blade.php
@@ -1,5 +1,6 @@
 @props(['action', 'method' => 'post'])
-<form action="{{ $action }}" method="{{ $method === 'get' ? 'get' : 'post' }}" class="py-8 px-16 rounded-2xl bg-neutral-100">
+<form action="{{ $action }}" method="{{ $method === 'get' ? 'get' : 'post' }}"
+      {{ $attributes->merge(['class' => 'py-8 px-16 rounded-2xl bg-neutral-100']) }}>
     @csrf
     @if(! in_array($method, ['post', 'get']))
         @method($method)
