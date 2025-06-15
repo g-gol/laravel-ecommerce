f4176787db2ed6ef6df9f61515ef3b489b806000
George Golubev <golubev.zhr@gmail.com>
2025-06-15 13:02:12 +0300

    feat(order): factories and seeding

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use App\Enums\Role;
+use App\Models\Order;
+use App\Models\OrderItem;
 use App\Models\Product;
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Arr;
@@ -35,10 +38,31 @@ class DatabaseSeeder extends Seeder
             $user->assignRole(Arr::random($roles));
         });
 
-        Product::factory(100)->create(function () use ($users) {
+        $products = Product::factory(100)->create(function () use ($users) {
             return [
                 'user_id' => $users->random()->id
             ];
+        });
+
+        Order::factory(200)->make()->each(function ($order) use ($products, $users) {
+            $order->user_id = $users->random()->id;
+            $order->save();
+
+            OrderItem::factory(3)->make()->each(function ($item) use ($products, $order) {
+                $product = $products->random();
+                $item->order_id = $order->id;
+                $item->product_id = $product->id;
+                $item->quantity = rand(1, 5);
+                $item->price = $product->price;
+
+                $item->save();
+            });
+
+            $total = $order->items->sum(function ($item) {
+                return $item->price * $item->quantity;
+            });
+            $order->total = $total;
+            $order->save();
         });
     }
 }
