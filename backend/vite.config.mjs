08cc8175b0b87f2c7223d9c160b2f3e0733733fb
George Golubev <golubev.zhr@gmail.com>
2025-06-17 10:59:39 +0300

    feat: alpine installed

--- backend/vite.config.mjs
+++ backend/vite.config.mjs
@@ -5,7 +5,7 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig ({
     plugins: [
         laravel({
-            input: ['resources/css/app.css'],
+            input: ['resources/css/app.css', 'resources/js/main.js'],
             refresh: true,
         }),
         tailwindcss(),
