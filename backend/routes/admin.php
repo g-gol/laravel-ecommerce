c6b12b8581e50918190a1b32b3da51e76c848fa8
George Golubev <golubev.zhr@gmail.com>
2025-06-18 10:10:25 +0300

    fix(orderItem): moved controller to the Admin namespace

--- backend/routes/admin.php
+++ backend/routes/admin.php
@@ -2,9 +2,9 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Admin\OrderItemController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\OrderItemController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('home');
